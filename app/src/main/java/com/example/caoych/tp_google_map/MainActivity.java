f72a3d52fe18d02cd84002b0821ad41cb62e657d
Jostar1024 <howard.eureka@gmail.com>
2016-11-14 12:13:34 +0100

    code inital avec layout

---
+++ app/src/main/java/com/example/caoych/tp_google_map/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.caoych.tp_google_map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch swZoom;
+    private Switch swRotate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swZoom = (Switch) findViewById(R.id.swZoom);
+        swRotate = (Switch) findViewById(R.id.swRotate);
+    }
+
+
+    public void jumpToMap(View view) {
+        // TODO : Communication entre MainActivy et MapsActivity
+    }
+}
+
---
+++ app/src/main/java/com/example/caoych/tp_google_map/MapsActivity.java
@@ -0,0 +1,54 @@
+package com.example.caoych.tp_google_map;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private float DEFAULT_ZOOM = 15;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        UiSettings settingMap = mMap.getUiSettings();
+
+        // TODO : Positionner la carte.
+
+        // TODO : Activier, Déactiver le zoom et rotation de la carte.
+
+        // TODO : Clic long pour ajouter un marker sur la carte.
+
+        // TODO : Clic pour changer la couleur de marker.
+
+        // TODO : Dessiner le Campus en polygone.
+    }
+
+}
